2ad407f309eedc5f0ef6d4f915f245e1a01fd2ae
ved727272 <brijesh.zte@gmail.com>
2025-08-27 23:07:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg1" {
-  name     = "latest-rg"
+  name     = "latest-rg3"
   location = "West Europe"
 }
 
 resource "azurerm_resource_group" "rg2" {
-  name     = "latest-rg2"
+  name     = "latest-rg4"
   location = "West Europe"
 }
 
